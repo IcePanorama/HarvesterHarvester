49a226812856251288c1e52bc5221dbe109e2d5b
IcePanorama <thriftedroot@protonmail.com>
2024-07-13 15:33:32 -0400

    Created dec_datetime typedef.

---
+++ src/dec_datetime.h
@@ -0,0 +1,16 @@
+#ifndef _DEC_DATETIME_H_
+#define _DEC_DATETIME_H_
+
+// See: https://wiki.osdev.org/ISO_9660#Date/time_format
+typedef struct dec_datetime {
+  char year[5];
+  char month[3];
+  char day[3];
+  char hour[3];
+  char minute[3];
+  char second[3];
+  char hundredths_of_a_second[3];
+  uint8_t time_zone_offset;
+} dec_datetime;
+
+#endif
--- src/volume_descriptor.h
+++ src/volume_descriptor.h
@@ -1,8 +1,12 @@
 #ifndef _VOLUME_DESCRIPTOR_H_
 #define _VOLUME_DESCRIPTOR_H_
 
+/* clang-format off */
 #include <stdint.h>
 #include <string.h>
+
+#include "dec_datetime.h"
+/* clang-format on */
 
 // See: https://wiki.osdev.org/ISO_9660#The_Primary_Volume_Descriptor
 typedef struct volume_descriptor_data {
@@ -25,10 +29,10 @@ typedef struct volume_descriptor_data {
   char copyright_file_identifier[38];
   char abstract_file_header[38];
   char bibliographic_file_identifier[38];
-  //dec_datetime volume_creation_date_and_time;
-  //dec_datetime volume_modification_date_and_time;
-  //dec_datetime volume_expiration_date_and_time;
-  //dec_datetime volume_effective_date_and_time;
+  dec_datetime volume_creation_date_and_time;
+  dec_datetime volume_modification_date_and_time;
+  dec_datetime volume_expiration_date_and_time;
+  dec_datetime volume_effective_date_and_time;
   uint8_t file_structure_version;                   // always `0x01`
 } volume_descriptor_data;
 
