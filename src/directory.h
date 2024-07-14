89d4230a65ae9db84ef4602f704346f5d1e9bf04
IcePanorama <thriftedroot@protonmail.com>
2024-07-14 17:07:25 -0400

    Added new datetime and file_flags to directory typedef.

--- src/directory.h
+++ src/directory.h
@@ -1,8 +1,13 @@
 #ifndef _DIRECTORY_H_
 #define _DIRECTORY_H_
 
+/* clang-format off */
 #include <stdint.h>
 #include <stdlib.h>
+
+#include "datetime.h"
+#include "file_flags.h"
+/* clang-format on */
 
 typedef struct directory_record
 {
@@ -10,8 +15,8 @@ typedef struct directory_record
   uint8_t extended_attribute_record_length;
   uint32_t location_of_extent;
   uint32_t data_length;
-  // recording date & time
-  // file flags
+  dir_datetime recording_datetime;
+  file_flags file_flags;
   uint8_t file_unit_size; // only used by files recorded in interleaved mode
   uint16_t volume_sequence_number;
   uint8_t file_identifier_length;
