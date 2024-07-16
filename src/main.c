5857ccc513fbfc63884def1c7f754c47c96ee41e
IcePanorama <thriftedroot@protonmail.com>
2024-07-16 19:07:42 -0400

    Refactored main.c.

--- src/main.c
+++ src/main.c
@@ -9,7 +9,6 @@
 #include "directory.h"
 #include "errors.h"
 #include "file_flags.h"
-#include "log.h"
 #include "output.h"
 #include "path_table.h"
 #include "utils.h"
@@ -19,10 +18,8 @@
 /* function prototypes */
 FILE *setup_extractor (char *filename);
 void process_DAT_file (FILE *fptr);
-// void print_hex_data (unsigned char *buffer, const uint8_t BUFFER_LEN);
 void process_volume_descriptor_header (FILE *fptr, volume_descriptor *vd);
 void process_volume_descriptor_data (FILE *fptr, volume_descriptor_data *vdd);
-// void print_some_data_from_file (FILE *fptr);
 void process_type_l_path_table (FILE *fptr, path_table *pt);
 void process_directory (FILE *fptr, directory *d);
 int8_t extract_file (FILE *fptr, directory_record *dr, const char *path);
@@ -151,38 +148,6 @@ process_DAT_file (FILE *fptr)
   // print_volume_descriptor_data (&vd.data);
 }
 
-// TODO: move this to a "logging" function or something like that.
-/*
- *  print_hex_data
- *
- *  Outputs hex data from a given `buffer` to stdout, formatting said data to
- *  add spaces between bytes, tabs after after every four bytes, and a new line
- *  after every 16 bytes.
- *
- *  param:
- *    buffer : unsigned char * - a buffer containing hex data.
- *    BUFFER_LEN : const uint8_t - the size of the data buffer.
- */
-/*
-void
-print_hex_data (unsigned char *buffer, const uint8_t BUFFER_LEN)
-{
-  for (uint8_t i = 0; i < BUFFER_LEN; i++)
-    {
-      printf (" %02x", buffer[i]);
-      if ((i != 0) && (i % 4 == 3))
-        {
-          printf ("\t");
-        }
-      if ((i != 0) && (i % 16 == 15))
-        {
-          puts ("");
-        }
-    }
-  puts ("");
-}
-*/
-
 /*
  *  process_volume_descriptor_header
  *
@@ -271,28 +236,6 @@ process_volume_descriptor_data (FILE *fptr, volume_descriptor_data *vdd)
 
   fseek (fptr, 0x28D, SEEK_CUR); // Reserved by ISO
 }
-
-/*
- *  print_some_data_from_file
- *
- *  Prints out the next `BYTES_TO_READ` many bytes to the stdout, formatted
- *  properly in order to be easy to read. Useful for debugging.
- */
-/*
-#define BYTES_TO_READ 32
-void
-print_some_data_from_file (FILE *fptr)
-{
-  unsigned char buffer[BYTES_TO_READ];
-  size_t bytes_read = fread (buffer, sizeof (buffer[0]), BYTES_TO_READ, fptr);
-  if (bytes_read != BYTES_TO_READ)
-    {
-      handle_fread_error (fptr, bytes_read, BYTES_TO_READ);
-    }
-  print_hex_data (buffer, BYTES_TO_READ);
-  fseek (fptr, -sizeof (buffer), SEEK_CUR);
-}
-*/
 
 /*
  *  process_type_l_path_table
