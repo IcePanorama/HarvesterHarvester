4cd14a2ac43e3cc8aee323865bc1a99a80cb43c4
IcePanorama <thriftedroot@protonmail.com>
2024-07-29 16:56:14 -0400

    Updated documentation for data_reader.h.

--- src/data_reader.c
+++ src/data_reader.c
@@ -1,3 +1,16 @@
+//  Copyright (C) 2024  IcePanorama
+//  This file is a part of HarvesterHarvester.
+//  HarvesterHarvester is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by the
+//  Free Software Foundation, either version 3 of the License, or (at your
+//  option) any later version.
+//  This program is distributed in the hope that it will be useful, but WITHOUT
+//  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+//  more details.
+//
+//  You should have received a copy of the GNU General Public License along
+//  with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include "data_reader.h"
 #include "errors.h"
 #include "file_flags.h"
@@ -5,8 +18,17 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-static int8_t read_big_endian_data_uint32_t (FILE *fptr, uint32_t *output);
+/**
+ *  Reads a uint16_t stored in big endian format from the file pointed to by
+ *  `fptr` and stores the result in `output`.
+ */
 static int8_t read_big_endian_data_uint16_t (FILE *fptr, uint16_t *output);
+
+/**
+ *  Reads a uint32_t stored in big endian format from the file pointed to by
+ *  `fptr` and stores the result in `output`.
+ */
+static int8_t read_big_endian_data_uint32_t (FILE *fptr, uint32_t *output);
 
 int8_t
 read_both_endian_data_uint32 (FILE *fptr, uint32_t *output)
--- src/data_reader.h
+++ src/data_reader.h
@@ -1,3 +1,23 @@
+/**
+ *  data_header.h - container for various data access functions.
+ *
+ *  All of these functions return zero on success, non-zero on failure. The
+ *  caller is responsible for freeing any memory which may or may not have been
+ *  allocated during the use of these functions.
+ */
+//  Copyright (C) 2024  IcePanorama
+//  This file is a part of HarvesterHarvester.
+//  HarvesterHarvester is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by the
+//  Free Software Foundation, either version 3 of the License, or (at your
+//  option) any later version.
+//  This program is distributed in the hope that it will be useful, but WITHOUT
+//  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+//  more details.
+//
+//  You should have received a copy of the GNU General Public License along
+//  with this program.  If not, see <https://www.gnu.org/licenses/>.
 #ifndef _DATA_READER_H_
 #define _DATA_READER_H_
 
@@ -7,48 +27,74 @@
 #include <stdint.h>
 #include <stdio.h>
 
+/**
+ *  Reads in a single uint8_t from a given file, storing the output in `value`.
+ */
+int8_t read_single_uint8 (FILE *fptr, uint8_t *value);
+
+/**
+ *  Reads a uint16_t stored in little endian format from the file pointed to by
+ *  `fptr` and stores the result in `output`.
+ */
+int8_t read_little_endian_data_uint16_t (FILE *fptr, uint16_t *output);
+
+/**
+ *  Reads a uint32_t stored in little endian format from the file pointed to by
+ *  `fptr` and stores the result in `output`.
+ */
 int8_t read_little_endian_data_uint32_t (FILE *fptr, uint32_t *output);
 
 /**
  *  When data in a given file is presented first in little endian form and then
  *  repeated in big endian form, this function will read both values, double
- *  checking that the first matches the second. This is almost certainly
- *  unnecessary, but it makes me feel better :).
+ *  checking that the first matches the second, and storing the result into
+ *  `output`. This is almost certainly unnecessary, but it makes me feel
+ *  better :).
  *
- *  FIXME: incorrect, need to rewrite
- *  @param  fptr  pointer to data to be read.
- *  @return uint32_t containing the read data.
- *  @see read_both_endian_data_unint16()
- */
-int8_t read_both_endian_data_uint32 (FILE *fptr, uint32_t *output);
-
-int8_t read_little_endian_data_uint16_t (FILE *fptr, uint16_t *output);
-
-/**
- *  When data in a given file is presented first in little endian form and then
- *  repeated in big endian form, this function will read both values, double
- *  checking that the first matches the second. This is almost certainly
- *  unnecessary, but it makes me feel better :).
- *
- * FIXME: incorrect, need to rewrite
- *  @param  fptr  pointer to data to be read.
- *  @return uint16_t containing the read data.
  *  @see read_both_endian_data_unint32()
  */
 int8_t read_both_endian_data_uint16 (FILE *fptr, uint16_t *output);
 
+/**
+ *  When data in a given file is presented first in little endian form and then
+ *  repeated in big endian form, this function will read both values, double
+ *  checking that the first matches the second, and and storing the result into
+ *  `output`. This is almost certainly unnecessary, but it makes me feel
+ *  better :).
+ *
+ *  @see read_both_endian_data_unint16()
+ */
+int8_t read_both_endian_data_uint32 (FILE *fptr, uint32_t *output);
+
+/**
+ *  Reads a string of a given length from a given file, storing the result in
+ *  `output`.
+ */
 int8_t read_string (FILE *fptr, char *output, uint8_t length);
+
+/**
+ *  Reads an array of uint8_t's, of a given length, from a given file,
+ *  storing the result in `output`.
+ */
 int8_t read_array_uint8 (FILE *fptr, uint8_t *arr, uint8_t length);
+
+/**
+ *  Reads in `dec_datetime` data from a given file, storing the output in `dt`.
+ *
+ *  @see dec_datetime
+ */
 int8_t read_dec_datetime (FILE *fptr, dec_datetime *dt);
-int8_t read_single_uint8 (FILE *fptr, uint8_t *value);
+
+/**
+ *  Reads in `dir_datetime` data from a given file, storing the output in `dt`.
+ *
+ *  @see dir_datetime
+ */
 int8_t read_dir_datetime (FILE *fptr, dir_datetime *dt);
 
 /**
- *  Reads file flag data in from a given source pointed to by `fptr` and uses
- *  that data to update the various flags in `ff` as needed.
+ *  Reads in `file_flag` data from a given file, storing the output in `ff`.
  *
- *  @param  fptr  Pointer to file with file flag data
- *  @param  ff    file flags to be updated.
  *  @see  file_flags
  */
 int8_t read_file_flags (FILE *fptr, file_flags *ff);
