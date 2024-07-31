60ba8a80fa19083b57cf3ed7b021b71f94dc8020
IcePanorama <thriftedroot@protonmail.com>
2024-07-31 11:17:37 -0400

    Improved documentation for output.h.

--- src/output.c
+++ src/output.c
@@ -1,3 +1,16 @@
+//  Copyright (C) 2024  IcePanorama
+//  This file is a part of HarvesterHarvester.
+//  HarvesterHarvester is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by the
+//  Free Software Foundation, either version 3 of the License, or (at your
+//  option) any later version.
+//  This program is distributed in the hope that it will be useful, but WITHOUT
+//  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+//  more details.
+//
+//  You should have received a copy of the GNU General Public License along
+//  with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include "output.h"
 #include "errors.h"
 #include "options.h"
@@ -56,7 +69,10 @@ create_output_directory (char *path)
 #endif
           if (status != 0)
             {
-              printf ("ERROR: failed to create directory, %s\n", path);
+              fprintf (stderr,
+                       "ERROR: failed to create the following output "
+                       "directory, %s\n.",
+                       path);
               free (dir);
               free (tmp);
               return HH_CREATE_OUTPUT_DIR_ERROR;
--- src/output.h
+++ src/output.h
@@ -1,9 +1,28 @@
-#ifndef _OUTPUT_H_
-#define _OUTPUT_H_
+//  Copyright (C) 2024  IcePanorama
+//  This file is a part of HarvesterHarvester.
+//  HarvesterHarvester is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by the
+//  Free Software Foundation, either version 3 of the License, or (at your
+//  option) any later version.
+//  This program is distributed in the hope that it will be useful, but WITHOUT
+//  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+//  more details.
+//
+//  You should have received a copy of the GNU General Public License along
+//  with this program.  If not, see <https://www.gnu.org/licenses/>.
+/** output.h - container for functions related to the output directory. */
+#ifndef _HH_OUTPUT_H_
+#define _HH_OUTPUT_H_
 
 #include <stdbool.h>
 #include <stdint.h>
 
+/**
+ *  Creates an output directory at the given path if it doesn't already exist.
+ *
+ *  @return zero on success, non-zero on failure.
+ */
 int8_t create_output_directory (char *path);
 
 #endif
