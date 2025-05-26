4121a607acb92b6d6f3f5fba4307592690d578d7
IcePanorama <thriftedroot@protonmail.com>
2025-05-26 13:06:07 -0400

    Refactored code.

--- include/iso_9660.h
+++ include/iso_9660.h
@@ -1,17 +1,76 @@
-#ifndef _ISO_9660_FILESYSTEM_H_
-#define _ISO_9660_FILESYSTEM_H_
+/**
+ *  iso_9660.h - an interface for processing and extracting ISO 9660/ECMA-119
+ *  file systems. All public functions are prefixed by `i9660` and all public
+ *  data types are prefixed by `ISO9660`.
+ *
+ *  Copyright (C) 2024-2025  IcePanorama
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#ifndef _ISO_9660_FILE_SYSTEM_H_
+#define _ISO_9660_FILE_SYSTEM_H_
 
 #include <stdio.h>
 
 typedef struct ISO9660FileSystem_s ISO9660FileSystem_t;
 
+/**
+ *  Allocates an ISO9660FileSystem_t. Said file system must later be freed via
+ *  `i9660_free`. Return value may be NULL upon calloc failure.
+ *
+ *  Return:  pointer to allocated i9660 file system.
+ *  See:  `i9660_free`
+ */
 ISO9660FileSystem_t *i9660_alloc (void);
+
+/**
+ *  Frees memory allocated by `fs`. If `fs` is NULL, this simply returns
+ *  immediately.
+ *
+ *  Param:  fs  a pointer to an ISO9660FileSystem_t
+ *  See:  `i9660_alloc`
+ */
 void i9660_free (ISO9660FileSystem_t *fs);
+
+/**
+ *  Prints all the data fields of `fs` to stdout. Returns immediately if `fs`
+ *  is NULL.
+ *
+ *  Param:  fs  some ISO9660FileSystem_t
+ */
 void i9660_print (ISO9660FileSystem_t *fs);
 
-/** Processes filesystem header and data sections. */
+/**
+ *  Initializes all the data fields of `fs` using the data provided via
+ *  `input_fptr`. Returns non-zero if `fs` is NULL.
+ *
+ *  Param:  fs          some ISO9660FileSystem_t
+ *  Param:  input_fptr  file pointer to some ISO 9660 data
+ *  Return:  Zero on success, non-zero on failure.
+ */
 int i9660_init (ISO9660FileSystem_t *fs, FILE input_fptr[static 1]);
+
+/**
+ *  Extracts `fs` using the data provided via `input_fptr` to the directory
+ *  pointed to by `path`. Returns non-zero if `fs` is NULL.
+ *
+ *  Param:  fs          some ISO9660FileSystem_t
+ *  Param:  input_fptr  file pointer to some ISO 9660 data
+ *  Param:  path        some directory where `fs` should be extracted.
+ *  Return:  Zero on success, non-zero on failure.
+ */
 int i9660_extract (ISO9660FileSystem_t *fs, FILE input_fptr[static 1],
                    const char path[static 1]);
 
-#endif /* _ISO_9660_FILESYSTEM_H_ */
+#endif /* _ISO_9660_FILE_SYSTEM_H_ */
--- src/iso_9660.c
+++ src/iso_9660.c
@@ -1,20 +1,36 @@
+/**
+ *  Copyright (C) 2024-2025  IcePanorama
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #include "iso_9660.h"
 #include "iso_9660/fs_header.h"
 #include "iso_9660/pri_vol_desc.h"
+#include "iso_9660/vol_desc_type_code.h"
 
 #include <stdint.h>
-#include <stdio.h>
 #include <stdlib.h>
 
-/** See: https://wiki.osdev.org/ISO_9660. */
+/** See:  https://wiki.osdev.org/ISO_9660. */
 struct ISO9660FileSystem_s
 {
   _FileSysHeader_t *header;
 
-  /** See: https://wiki.osdev.org/ISO_9660#Volume_Descriptors. */
+  /** See:  https://wiki.osdev.org/ISO_9660#Volume_Descriptors. */
   union _VolDescData_u
   {
-    /** See: https://wiki.osdev.org/ISO_9660#The_Boot_Record. */
+    /** See:  https://wiki.osdev.org/ISO_9660#The_Boot_Record. */
     struct _BootRecVolDesc_s
     {
       char boot_system_identifier[32];
@@ -27,18 +43,16 @@ struct ISO9660FileSystem_s
     /**
      *  Volume Descriptor Set Terminator data "does not define bytes 7-2047 of
      *  its Volume Descriptor."
-     *  See: https://wiki.osdev.org/ISO_9660#Volume_Descriptor_Set_Terminator
-     *  TODO: decide: is it even worth creating a struct for this then?
-     *  In theory, we could put this into a (u)int8_t array.
+     *  See:  https://wiki.osdev.org/ISO_9660#Volume_Descriptor_Set_Terminator.
      */
+    uint8_t vol_desc_set_term_data[2041];
   } vol_desc_data;
 };
 
 ISO9660FileSystem_t *
 i9660_alloc (void)
 {
-  ISO9660FileSystem_t *output = calloc (1, sizeof (ISO9660FileSystem_t));
-  return output;
+  return calloc (1, sizeof (ISO9660FileSystem_t));
 }
 
 void
@@ -53,58 +67,15 @@ i9660_free (ISO9660FileSystem_t *fs)
       _pvd_free (fs->vol_desc_data.pri_vol_desc);
       break;
     default:
+      fprintf (stderr,
+               "There is currently no support for freeing volume descriptor "
+               "data of type %02X.\n",
+               _fsh_get_vol_desc_type_code (fs->header));
       break;
     }
 
   _fsh_free (fs->header);
   free (fs);
-}
-
-static int
-process_vol_desc_data (enum _VolDescTypeCode_e type,
-                       union _VolDescData_u *data, FILE *input_fptr)
-{
-  switch (type)
-    {
-    case _VDTC_PRIMARY_VOLUME:
-      data->pri_vol_desc = _pvd_alloc ();
-      if ((data->pri_vol_desc == NULL)
-          || (_pvd_init (data->pri_vol_desc, input_fptr) != 0))
-        return -1;
-      break;
-    default:
-      fprintf (
-          stderr,
-          "Support for initializing type code %02X isn't implemented yet.\n",
-          type);
-      return -1;
-    }
-
-  return 0;
-}
-
-int
-i9660_init (ISO9660FileSystem_t *fs, FILE input_fptr[static 1])
-{
-  if (fs == NULL)
-    return -1;
-
-  if (fseek (input_fptr, 0x8000, SEEK_SET) != 0)
-    {
-      fprintf (stderr, "ERROR: failed to seek past system area (32KiB).\n");
-      return -1;
-    }
-
-  fs->header = _fsh_alloc ();
-  if ((fs->header == NULL) || (_fsh_init (fs->header, input_fptr) != 0))
-    return -1;
-
-  if (process_vol_desc_data (_fsh_get_vol_desc_type_code (fs->header),
-                             &fs->vol_desc_data, input_fptr)
-      != 0)
-    return -1;
-
-  return 0;
 }
 
 void
@@ -114,17 +85,57 @@ i9660_print (ISO9660FileSystem_t *fs)
     return;
 
   _fsh_print (fs->header);
-  switch (_fsh_get_vol_desc_type_code (fs->header))
+  _VolDescTypeCode_t type = _fsh_get_vol_desc_type_code (fs->header);
+  switch (type)
     {
     case _VDTC_PRIMARY_VOLUME:
       _pvd_print (fs->vol_desc_data.pri_vol_desc);
       break;
     default:
       fprintf (stderr,
-               "Support for printing type code %02X isn't implemented yet.\n",
-               _fsh_get_vol_desc_type_code (fs->header));
+               "There is currently no support for printing volume descriptor "
+               "data of type %02X.\n",
+               type);
       break;
     }
+}
+
+int
+i9660_init (ISO9660FileSystem_t *fs, FILE input_fptr[static 1])
+{
+  if (fs == NULL)
+    return -1;
+
+  // See:  https://wiki.osdev.org/ISO_9660#System_Area.
+  if (fseek (input_fptr, 0x8000, SEEK_SET) != 0)
+    {
+      fprintf (stderr, "ERROR: failed to seek past system area.\n");
+      return -1;
+    }
+
+  fs->header = _fsh_alloc ();
+  if ((fs->header == NULL) || (_fsh_init (fs->header, input_fptr) != 0))
+    return -1;
+
+  union _VolDescData_u *data = &fs->vol_desc_data;
+  _VolDescTypeCode_t type = _fsh_get_vol_desc_type_code (fs->header);
+  switch (type)
+    {
+    case _VDTC_PRIMARY_VOLUME:
+      data->pri_vol_desc = _pvd_alloc ();
+      if ((data->pri_vol_desc == NULL)
+          || (_pvd_init (data->pri_vol_desc, input_fptr) != 0))
+        return -1;
+      break;
+    default:
+      fprintf (stderr,
+               "There is currently no support for initializing volume "
+               "descriptor data of type %02X.\n",
+               type);
+      return -1;
+    }
+
+  return 0;
 }
 
 int
@@ -134,7 +145,8 @@ i9660_extract (ISO9660FileSystem_t *fs, FILE input_fptr[static 1],
   if (fs == NULL)
     return -1;
 
-  switch (_fsh_get_vol_desc_type_code (fs->header))
+  _VolDescTypeCode_t type = _fsh_get_vol_desc_type_code (fs->header);
+  switch (type)
     {
     case _VDTC_PRIMARY_VOLUME:
       if (_pvd_extract (fs->vol_desc_data.pri_vol_desc, input_fptr, path) != 0)
@@ -142,9 +154,9 @@ i9660_extract (ISO9660FileSystem_t *fs, FILE input_fptr[static 1],
       break;
     default:
       fprintf (stderr,
-               "No implemented support for extracting file systems of type "
-               "code %02X.\n",
-               _fsh_get_vol_desc_type_code (fs->header));
+               "There is currently no support for extracting volume "
+               "descriptor data of type %02X.\n",
+               type);
       return -1;
     }
 
