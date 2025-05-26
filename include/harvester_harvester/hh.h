079aab00b7ff2915b6a537f612823110eea9c974
IcePanorama <thriftedroot@protonmail.com>
2025-05-26 14:22:42 -0400

    Update documentation

--- include/harvester_harvester/hh.h
+++ include/harvester_harvester/hh.h
@@ -1,7 +1,8 @@
 /**
  *  harvester_harvester/hh.h - an interface for extracting data from the .dat
  *  files of DigiFX Interactive's 1996 game, Harvester. All public functions
- *  and data fields are prefixed by `hh`. Data types are prefixed by `HH`.
+ *  and data fields are prefixed by `hh`. All public data types are prefixed by
+ *  `HH`.
  *
  *  Copyright (C) 2024-2025  IcePanorama
  *
