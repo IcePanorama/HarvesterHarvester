4e07d0187f377490c3dc32e765353fc29754bef2
IcePanorama <thriftedroot@protonmail.com>
2025-05-26 12:04:48 -0400

    refactored code.

--- include/harvester_harvester/hh.h
+++ include/harvester_harvester/hh.h
@@ -1,3 +1,22 @@
+/**
+ *  harvester_harvester/hh.h - an interface for extracting data from the .dat
+ *  files of DigiFX Interactive's 1996 game, Harvester.
+ *
+ *  Copyright (C) 2024-2025  IcePanorama
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #ifndef _HARVESTER_HARVESTER_H_
 #define _HARVESTER_HARVESTER_H_
 
@@ -6,21 +25,28 @@
 extern const HHOptions_t HH_DEFAULT_OPTIONS;
 
 /**
+ *  Extracts the given i9660 or Harvester file system (`input_path`) to the
+ *  provided `output_path` using `opts`.
+ *
+ *  param:  input_path   path to some dat file.
+ *  param:  output_path  directory where extracted data should be placed.
+ *  param:  opts         customized options for extraction
+ *  returns: zero on success, non-zero on failure.
+ */
+int hh_extract_filesystem_w_options (const char input_path[static 1],
+                                     const char output_path[static 1],
+                                     const HHOptions_t opts);
+
+/**
+ *  Extracts the given i9660 or Harvester file system (`input_path`) to the
+ *  provided `output_path` using `HH_DEFAULT_OPTIONS`. Internally, this just
+ *  calls `hh_extract_filesystem_w_options`.
+ *
  *  param:  input_path   path to some dat file.
  *  param:  output_path  directory where extracted data should be placed.
  *  returns: zero on success, non-zero on failure.
  */
 int hh_extract_filesystem (const char input_path[static 1],
                            const char output_path[static 1]);
-
-/**
- *  param:  input_path   path to some dat file.
- *  param:  output_path  directory where extracted data should be placed.
- *  param:  options      customized options for extraction
- *  returns: zero on success, non-zero on failure.
- */
-int hh_extract_filesystem_w_options (const char input_path[static 1],
-                                     const char output_path[static 1],
-                                     const HHOptions_t opts);
 
 #endif /* _HARVESTER_HARVESTER_H_ */
--- src/harvester_harvester/hh.c
+++ src/harvester_harvester/hh.c
@@ -1,47 +1,30 @@
+/**
+ *  Copyright (C) 2024-2025  IcePanorama
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #include "harvester_harvester/hh.h"
 #include "harvester_harvester/idx_file.h"
 #include "harvester_harvester/known_files.h"
-#include "harvester_harvester/options.h"
 #include "iso_9660.h"
 
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 const HHOptions_t HH_DEFAULT_OPTIONS
     = { .processing_mode = _HHPM_NORMAL_PROCESSING };
-
-static int
-extract_i9660_fs (const char *input_path, const char *output_path)
-{
-  ISO9660FileSystem_t *fs = i9660_alloc ();
-  if (fs == NULL)
-    {
-      fprintf (stderr, "Out of memory error.\n");
-      return -1;
-    }
-
-  FILE *input_file = fopen (input_path, "rb");
-  if (input_file == NULL)
-    {
-      fprintf (stderr, "Error opening file %s.\n", input_path);
-      i9660_free (fs);
-      return -1;
-    }
-
-  if ((i9660_init (fs, input_file) != 0)
-      || (i9660_extract (fs, input_file, output_path) != 0))
-    {
-      i9660_free (fs);
-      fclose (input_file);
-      return -1;
-    }
-
-  i9660_free (fs);
-  fclose (input_file);
-  return 0;
-}
 
 static int
 extract_int_dat (const char *output_path, const char *dat_path,
@@ -92,39 +75,6 @@ extract_int_dat (const char *output_path, const char *dat_path,
 oom_error:
   fprintf (stderr, "%s: Out of memory error.\n", __func__);
   return -1;
-}
-
-int
-hh_extract_filesystem (const char input_path[static 1],
-                       const char output_path[static 1])
-{
-  return hh_extract_filesystem_w_options (input_path, output_path,
-                                          HH_DEFAULT_OPTIONS);
-}
-
-static int
-known_i9660_extraction (const char input_path[static 1],
-                        const char output_path[static 1],
-                        const HHOptions_t opts)
-{
-  if (extract_i9660_fs (input_path, output_path) != 0)
-    return -1;
-
-  if (opts.processing_mode == _HHPM_SKIP_INTERNAL_DATS)
-    return 0;
-
-  const char **int_dat_files = _hhkf_get_int_dat_paths_from_i9660 (input_path);
-  const char **idx_files = _hhkf_get_int_idx_paths_from_i9660 (input_path);
-  if ((int_dat_files == NULL) || (idx_files == NULL))
-    return -1;
-
-  for (size_t i = 0; i < 3; i++)
-    {
-      if (extract_int_dat (output_path, int_dat_files[i], idx_files[i]) != 0)
-        return -1;
-    }
-
-  return 0;
 }
 
 /**
@@ -188,6 +138,61 @@ extraction_w_int_dat_input (const char input_path[static 1],
   return 0;
 }
 
+static int
+extract_i9660_fs (const char *input_path, const char *output_path)
+{
+  ISO9660FileSystem_t *fs = i9660_alloc ();
+  if (fs == NULL)
+    {
+      fprintf (stderr, "Out of memory error.\n");
+      return -1;
+    }
+
+  FILE *input_file = fopen (input_path, "rb");
+  if (input_file == NULL)
+    {
+      fprintf (stderr, "Error opening file %s.\n", input_path);
+      i9660_free (fs);
+      return -1;
+    }
+
+  if ((i9660_init (fs, input_file) != 0)
+      || (i9660_extract (fs, input_file, output_path) != 0))
+    {
+      i9660_free (fs);
+      fclose (input_file);
+      return -1;
+    }
+
+  i9660_free (fs);
+  fclose (input_file);
+  return 0;
+}
+
+static int
+known_i9660_extraction (const char *input_path, const char *output_path,
+                        const HHOptions_t *opts)
+{
+  if (extract_i9660_fs (input_path, output_path) != 0)
+    return -1;
+
+  if (opts->processing_mode == _HHPM_SKIP_INTERNAL_DATS)
+    return 0;
+
+  const char **int_dat_files = _hhkf_get_int_dat_paths_from_i9660 (input_path);
+  const char **idx_files = _hhkf_get_int_idx_paths_from_i9660 (input_path);
+  if ((int_dat_files == NULL) || (idx_files == NULL))
+    return -1;
+
+  for (size_t i = 0; i < 3; i++)
+    {
+      if (extract_int_dat (output_path, int_dat_files[i], idx_files[i]) != 0)
+        return -1;
+    }
+
+  return 0;
+}
+
 int
 hh_extract_filesystem_w_options (const char input_path[static 1],
                                  const char output_path[static 1],
@@ -196,7 +201,7 @@ hh_extract_filesystem_w_options (const char input_path[static 1],
   if ((opts.processing_mode != _HHPM_SKIP_I9660_DATS)
       && (_hhkf_is_known_i9660_file (input_path)))
     {
-      return known_i9660_extraction (input_path, output_path, opts);
+      return known_i9660_extraction (input_path, output_path, &opts);
     }
   else if (opts.processing_mode == _HHPM_SKIP_I9660_DATS)
     {
@@ -214,4 +219,12 @@ hh_extract_filesystem_w_options (const char input_path[static 1],
     }
 
   return 0;
+}
+
+int
+hh_extract_filesystem (const char input_path[static 1],
+                       const char output_path[static 1])
+{
+  return hh_extract_filesystem_w_options (input_path, output_path,
+                                          HH_DEFAULT_OPTIONS);
 }
