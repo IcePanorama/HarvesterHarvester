dd7ef07db0156b2e60f935ff4c9e7394be597547
IcePanorama <thriftedroot@protonmail.com>
2025-06-04 10:55:40 -0400

    Refactored code.

---
+++ tools/verify-chksums/checksum_dict.py
@@ -0,0 +1,87 @@
+import logging
+
+
+class ChecksumDict:
+    """
+    A dictionary wrapper class which correlates file paths to checksum values.
+    Designed to work in conjunction with `batch-md5`.
+
+    Attributes:
+        _dict: An dictionary used internal for storing said correlations.
+        _filename: The filename of the file used to create this instance.
+    """
+    class ChecksumMismatchError(Exception):
+        """A custom exception for checksum mismatches."""
+
+        def __init__(self, filename: str, original: str, new: str) -> None:
+            super().__init__(f"Checksum mismatch for '{filename}' (current: "
+                             + f"{original}, new: {new}).")
+
+    def __init__(self, input_path: str) -> None:
+        """
+        Initializes an instance using an input file.
+
+        Args:
+            input_path (str): path to some checksum file.
+
+        Raises:
+            RuntimeError: If line in file is malformed or duplicate key is
+            already in dict.
+        """
+        self._dict: dict(str, str) = {}
+        self._filename: str = input_path
+        with open(input_path) as fptr:
+            for line in fptr:
+                if not line:
+                    continue
+
+                segments = line.split("\t")
+                if (len(segments) < 2):
+                    raise RuntimeError(f"Bad input: {line}")
+                segments[1] = segments[1][:-1]
+                if (segments[1] in self._dict):
+                    raise RuntimeError(f"Duplicate key in {input_path}: " +
+                                       f"{segments[1]} => " +
+                                       f"{self._dict[segments[1]]}")
+
+                self._dict[segments[1]] = segments[0]
+
+    def __len__(self) -> int:
+        """Overrides default len implementation."""
+        return len(self._dict)
+
+    def __eq__(self, o: 'ChecksumDict') -> bool:
+        """Overrides default implementation."""
+        if isinstance(o, ChecksumDict):
+            return self._compare_chksum_dicts(o)
+        return NotImplemented
+
+    def __ne__(self, o: 'ChecksumDict') -> bool:
+        """Overrides default implementation."""
+        out = self.__eq__(o)
+        if out is NotImplemented:
+            return NotImplemented
+        return not out
+
+    def _compare_chksum_dicts(self, o: 'ChecksumDict') -> bool:
+        """
+        Handles the logic of comparing two checksum dictionaries.
+
+        Args:
+            o: Another ChecksumDict to compare `self` with.
+
+        Raises:
+            ChecksumMismatchError: If checksums in `self` differs from `o`.
+        """
+        if len(self) > len(o):
+            return False
+
+        for file, chksum in o._dict.items():
+            if file not in self._dict:
+                logging.warn(f"Warning: file, {file}, not found in "
+                             + f"{o._filename}.")
+            elif self._dict[file] != chksum:
+                raise self.ChecksumMismatchError(
+                    file, self._dict[file], chksum)
+
+        return True
