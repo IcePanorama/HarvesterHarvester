3b72cba7adbe667414187a0d7d40e95412495b25
IcePanorama <thriftedroot@protonmail.com>
2025-06-04 15:36:01 -0400

    Removed empty comment

--- include/harvester_harvester/idx_file.h
+++ include/harvester_harvester/idx_file.h
@@ -37,8 +37,6 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-/**
- */
 #ifndef _HARVESTER_HARVESTER_INDEX_FILE_H_
 #define _HARVESTER_HARVESTER_INDEX_FILE_H_
 
