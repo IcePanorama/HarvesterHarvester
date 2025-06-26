b612fab62558b4a8ba8fe1c096b98ca660e4809d
IcePanorama <thriftedroot@protonmail.com>
2025-06-26 10:18:39 -0400

    Refactored code, added 'set -e' line for halting upon err.

--- tools/make_releases.sh
+++ tools/make_releases.sh
@@ -1,41 +1,39 @@
 # make_releases - a script for generating every possible version of
 # HarvesterHarvester.
+#
 # This script basically just runs each `X.cmake` toolchain file in `./.cmake`.
 # It's implementation is quite specific to my current setup, however, that
 # should be fine as I'll likely be the only person to ever actually make
 # release builds.
 #! /usr/bin/env bash
+set -e
+
 BUILD_DIR=build
 HH_OUTPUT_DIR=output
-
-function setup ()
-{
-	rm -rf ${BUILD_DIR} &> /dev/null
-	mkdir ${BUILD_DIR} &> /dev/null
-	cd ${BUILD_DIR}
-}
-
-function tear_down ()
-{
-	make full
-	cd ..
-}
+CHKSUM_EXPECTED_OUTPUT=./.github/full_output_checksums.txt
 
 function build ()
 {
-	setup
+	# Setup
+	rm -rf ${BUILD_DIR} &> /dev/null
+	mkdir ${BUILD_DIR} &> /dev/null
+	cd ${BUILD_DIR}
+
 	if [[ -z $2 ]]; then
 		cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=$1 ..
 	else
 		cmake -DCMAKE_C_COMPILER=$2 -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=$1 ..
 	fi
-	tear_down
+
+	# Tear-down
+	make full
+	cd ..
 }
 
 function run_tests ()
 {
-	python ./tools/batch-md5 output
-	python ./tools/verify-chksums ./.github/full_output_checksums.txt checksums.txt
+	python ./tools/batch-md5 ${HH_OUTPUT_DIR}
+	python ./tools/verify-chksums ${CHKSUM_EXPECTED_OUTPUT} checksums.txt
 }
 
 rm -rf ${HH_OUTPUT_DIR} &> /dev/null
